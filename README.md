d92f2a631e2dc500055d3ae8e94182cbe75cede8
John Moris <jonathanspencermorris@gmail.com>
2025-03-21 00:45:12 +0000

    feat: update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # The Movies
 
-Syndica coding challenge
+## Introduction
+
+Create Axum server with the following endpoints:
+
+1. GET `/movie/{id}` - This should return a movie given the ID
+2. POST `/movie` - this should save move in a DB (`HashMap<String, Movie>`). This movie will be sent via a JSON payload.
+
+As a bonus: implement a caching layer, so we don't need to make expensive "DB" lookups, etc.
--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,6 @@ struct AppState {
 
 #[tokio::main]
 async fn main() {
-    // Create Axum server with the following endpoints:
-    // 1. GET /movie/{id} - This should return back a movie given the id
-    // 2. POST /movie - this should save move in a DB (HashMap<String, Movie>). This movie will be sent
-    // via a JSON payload.
-    // As a bonus: implement a caching layer so we don't need to make expensive "DB" lookups, etc.
-
     let data: HashMap<String, Movie> = HashMap::new();
     let state = AppState {
         data: Arc::new(Mutex::new(data)),
